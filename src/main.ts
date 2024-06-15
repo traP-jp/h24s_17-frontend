adb2a492975473322fc4b19e8972ad30215de06a
itt828 <iittt828@gmail.com>
2024-06-15 18:24:29 +0900

    feat: intro leaflet

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import 'leaflet/dist/leaflet.css';
 
 const app = createApp(App);
 app.use(router);
--- src/pages/mobile/HomePage.vue
+++ src/pages/mobile/HomePage.vue
@@ -1,132 +1,23 @@
 <template>
-  <div style="height: 100vw; width: 100vw;">
-    <l-map
-      v-model="zoom"
-      v-model:zoom="zoom"
-      :center="[47.41322, -1.219482]"
-      @move="log('move')"
-    >
-      <l-tile-layer
+  <div style="height: 100vw; width: 100vw">
+    <LMap v-model:zoom="zoom" :center="center" :use-global-leaflet="false">
+      <LTileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      ></l-tile-layer>
-      <l-control-layers />
-      <l-marker :lat-lng="[35.606895, 139.684874]" draggable @moveend="log('moveend')">
-        <l-tooltip>
-          東工大大岡山キャンパス
-        </l-tooltip>
-      </l-marker>
-
-      <l-marker :lat-lng="[47.41322, -1.219482]">
-        <l-icon :icon-url="iconUrl" :icon-size="iconSize" />
-      </l-marker>
-
-      <l-marker :lat-lng="[50, 50]" draggable @moveend="log('moveend')">
-        <l-popup>
-          lol
-        </l-popup>
-      </l-marker>
-
-      <l-polyline
-        :lat-lngs="[
-          [47.334852, -1.509485],
-          [47.342596, -1.328731],
-          [47.241487, -1.190568],
-          [47.234787, -1.358337],
-        ]"
-        color="green"
-      ></l-polyline>
-      <l-polygon
-        :lat-lngs="[
-          [46.334852, -1.509485],
-          [46.342596, -1.328731],
-          [46.241487, -1.190568],
-          [46.234787, -1.358337],
-        ]"
-        color="#41b782"
-        :fill="true"
-        :fillOpacity="0.5"
-        fillColor="#41b782"
-      />
-      <l-rectangle
-        :lat-lngs="[
-          [46.334852, -1.509485],
-          [46.342596, -1.328731],
-          [46.241487, -1.190568],
-          [46.234787, -1.358337],
-        ]"
-        :fill="true"
-        color="#35495d"
-      />
-      <l-rectangle
-        :bounds="[
-          [46.334852, -1.190568],
-          [46.241487, -1.090357],
-        ]"
-      >
-        <l-popup>
-          lol
-        </l-popup>
-      </l-rectangle>
-    </l-map>
-    <button @click="changeIcon">New kitten icon</button>
+      ></LTileLayer>
+      <LMarker :lat-lng="[35.606895, 139.684874]">
+        <LTooltip> 東工大大岡山キャンパス </LTooltip>
+      </LMarker>
+    </LMap>
   </div>
 </template>
-<script>
-import {
-  LMap,
-  LIcon,
-  LTileLayer,
-  LMarker,
-  LControlLayers,
-  LTooltip,
-  LPopup,
-  LPolyline,
-  LPolygon,
-  LRectangle,
-} from "@vue-leaflet/vue-leaflet";
+
+<script setup lang="ts">
+import { LMap, LTileLayer, LMarker, LTooltip } from "@vue-leaflet/vue-leaflet";
+import { type PointTuple } from "leaflet";
 import "leaflet/dist/leaflet.css";
+import { ref } from "vue";
 
-export default {
-  components: {
-    LMap,
-    LIcon,
-    LTileLayer,
-    LMarker,
-    LControlLayers,
-    LTooltip,
-    LPopup,
-    LPolyline,
-    LPolygon,
-    LRectangle,
-  },
-  data() {
-    return {
-      zoom: 2,
-      iconWidth: 25,
-      iconHeight: 40,
-    };
-  },
-  computed: {
-    iconUrl() {
-      return `https://placekitten.com/${this.iconWidth}/${this.iconHeight}`;
-    },
-    iconSize() {
-      return [this.iconWidth, this.iconHeight];
-    },
-  },
-  methods: {
-    log(a) {
-      console.log(a);
-    },
-    changeIcon() {
-      this.iconWidth += 2;
-      if (this.iconWidth > this.iconHeight) {
-        this.iconWidth = Math.floor(this.iconHeight / 2);
-      }
-    },
-  },
-};
+const zoom = ref(20);
+const center = ref<PointTuple>([35.606895, 139.684874]);
 </script>
-<style>
-
-</style>
+<style></style>
