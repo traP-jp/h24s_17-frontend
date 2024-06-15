ebb79d343d04216273a955785b640da503d5b4c4
PL-38 <hmhm.300337@gmail.com>
2024-06-15 10:49:16 +0900

    I added what we can download image.

--- src/pages/mac/WelcomePage.vue
+++ src/pages/mac/WelcomePage.vue
@@ -1,2 +1,9 @@
 <template></template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div>QRコードを読み取ってチェックイン</div>
+    <img :src="istockphoto-1128165817-612x612.jpg" alt="Description">
+</template>
