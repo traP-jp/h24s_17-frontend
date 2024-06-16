ce45cdaaf517a1a561839fe94f255ddd7ab86db1
itt828 <iittt828@gmail.com>
2024-06-16 20:12:42 +0900

    fix: fix js

--- src/pages/mac/WelcomePage.vue
+++ src/pages/mac/WelcomePage.vue
@@ -10,15 +10,16 @@ const token = await fetch(`/api/stand`, {
   },
 })
   .then((v) => v.json())
-  .then((v) => v.token);
+  .then((v) => v["token"]);
+// const token = "hoge";
 const checkInLink = `https://h24s17.trap.show/checkin?token=${token}`;
 </script>
 
 <template>
-  <h1>24年度春ハッカソン会場 sponserd by Engineer guildへようこそ！</h1>
-  <!-- <h1>{{ checkInLink }}</h1> -->
-  <h2>QRコードを読み取ってチェックイン</h2>
   <div :class="$style.qrContainer">
+    <h1>24年度春ハッカソン会場 sponserd by Engineer guildへようこそ！</h1>
+    <!-- <h1>{{ checkInLink }}</h1> -->
+    <h2>QRコードを読み取ってチェックイン</h2>
     <qrcode-vue :value="checkInLink" level="M" render-as="svg" :size="280" />
   </div>
 </template>
@@ -27,5 +28,6 @@ const checkInLink = `https://h24s17.trap.show/checkin?token=${token}`;
 .qrContainer {
   display: grid;
   justify-content: center;
+  justify-items: center;
 }
 </style>
