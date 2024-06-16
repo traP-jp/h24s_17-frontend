119622dd51be92a8dce0fd4f1baf7b8104547761
itt828 <iittt828@gmail.com>
2024-06-16 11:18:12 +0900

    feat: update checkin

--- src/pages/mobile/CheckinPage.vue
+++ src/pages/mobile/CheckinPage.vue
@@ -1,16 +1,37 @@
 <template>
-  <div>oisu</div>
-  {{ token }}
+  <v-dialog
+    v-model="showMordal"
+    transition="dialog-bottom-transition"
+    persistent
+  >
+    <v-card>
+      <div>{{ "foo" }}さん！ ハッカソンへようこそ！</div>
+      <div>記念撮影をしてね！</div>
+    </v-card>
+  </v-dialog>
+  <div v-if="!showMordal">
+    <v-progress-circular model-value="20" indeterminate></v-progress-circular>
+  </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from "vue";
 import { useRoute, useRouter } from "vue-router";
 
+const showMordal = ref(false);
 const { query } = useRoute();
 const router = useRouter();
 const token = query.token;
-await fetch(`/api/checkin?token=${token}`, {
-  method: "POST",
-});
-router.push("/");
+const userId = ref("");
+(async () => {
+  const response = await fetch(`/api/checkin?token=${token}`, {
+    method: "POST",
+  });
+
+  userId.value = response.headers.get("X-Forwarded-User") || "unknown";
+  showMordal.value = true;
+  await new Promise((resolve) => setTimeout(resolve, 5000));
+
+  router.push("/");
+})();
 </script>
