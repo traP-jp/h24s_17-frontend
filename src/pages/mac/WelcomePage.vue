a827a44292fba71cbfa371e210293da9bd357c45
itt828 <iittt828@gmail.com>
2024-06-16 17:18:17 +0900

    fix: use query param for mac secret

--- src/pages/mac/WelcomePage.vue
+++ src/pages/mac/WelcomePage.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts">
 import QrcodeVue from "qrcode.vue";
+import { useRoute } from "vue-router";
 
-const token = await fetch("/api/stand")
+const { query } = useRoute();
+const secret = query.secret;
+const token = await fetch(`/api/stand?secret=${secret}`)
   .then((v) => v.json())
   .then((v) => v.token);
 const checkInLink = `https://h24s17.trap.show/checkin?token=${token}`;
