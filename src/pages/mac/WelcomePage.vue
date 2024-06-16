1b267c9aace9668d3c8dbe142416e5719534c34f
itt828 <iittt828@gmail.com>
2024-06-16 20:45:49 +0900

    use sokuji kansu

--- src/pages/mac/WelcomePage.vue
+++ src/pages/mac/WelcomePage.vue
@@ -1,17 +1,21 @@
 <script setup lang="ts">
 import QrcodeVue from "qrcode.vue";
+import { ref } from "vue";
 import { useRoute } from "vue-router";
 
 const { query } = useRoute();
-const secret = query.secret as string;
-const resp = await fetch(`/api/stand`, {
-  headers: {
-    "X-Mac-Secret": secret,
-  },
-}).then((v) => v.json());
-const token = resp["token"];
-// const token = "hoge";
-const checkInLink = `https://h24s17.trap.show/checkin?token=${token}`;
+const checkInLink = ref<string>("");
+(async () => {
+  const secret = query.secret as string;
+  const resp = await fetch(`/api/stand`, {
+    headers: {
+      "X-Mac-Secret": secret,
+    },
+  }).then((v) => v.json());
+  const token = resp["token"];
+  // const token = "hoge";
+  checkInLink.value = `https://h24s17.trap.show/checkin?token=${token}`;
+})();
 </script>
 
 <template>
